eeea2a9c60700f6725ad9abd172719977e6ae5c0
Bob Zurad <bob@zurad.net>
2025-04-18 23:38:34 -0500

    implemented DEL and ENTER key logic

--- src/app/components/Key.tsx
+++ src/app/components/Key.tsx
@@ -9,22 +9,50 @@ export default function Key(props: { value: string }) {
   const [state, setState] = useAtom(mainAtom);
 
   const keyClick = (e: any) => {
-    if (state.currentGuessLetters.length === state.wordLength) {
+    if (
+      (value.toUpperCase() === 'DEL' &&
+        state.currentGuessLetters.length === 0) ||
+      (value.toUpperCase() === 'ENTER' &&
+        state.currentGuessLetters.length !== state.wordLength) ||
+      (value.toUpperCase() !== 'DEL' &&
+        value.toUpperCase() !== 'ENTER' &&
+        state.currentGuessLetters.length === state.wordLength)
+    ) {
+      // we've reached one of the following invalid use cases:
+      // - delete key was pressed when there's nothing to delete
+      // - enter key was pressed before filling the row
+      // - letter key was pressed after filling the row
       return;
     }
 
-    state.currentGuessLetters.push(value);
+    console.log(`key pressed: ${value}`);
+
+    if (value.toUpperCase() === 'DEL') {
+      // delete key was pressed
+      state.currentGuessLetters.pop();
+    } else if (value.toUpperCase() === 'ENTER') {
+      // enter key was pressed
+      state.guesses.push(state.currentGuessLetters);
+      state.currentGuessLetters = [];
+      state.numberOfGuessesRemaining--;
+    } else {
+      // letter key was pressed
+      state.currentGuessLetters.push(value);
+    }
+
+    console.log(`setting updated state...`);
     console.log(state);
-    // TODO: do this when enter key is pressed
-    // if (!state.guesses[currentRow]) {
-    //   state.guesses.push(state.currentGuessLetters);
-    // }
+
     setState({ ...state });
   };
 
   return (
     <>
-      <button className="keyboard-button" onClick={keyClick}>
+      <button
+        className="keyboard-button"
+        onClick={keyClick}
+        disabled={state.numberOfGuessesRemaining === 0}
+      >
         {value}
       </button>
     </>
